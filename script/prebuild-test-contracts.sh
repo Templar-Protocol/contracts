b391e555d1d4713e48bcc696e5d9cfa5d5b4517b
peer2 <peer2@templarprotocol.com>
2025-03-24 16:37:30 +0900

    chore: fix dirs and dedup to prebuild-test-contracts.sh

---
+++ script/prebuild-test-contracts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+
+cd "$ROOT_DIR/mock/oracle"
+cargo near build non-reproducible-wasm 1>&2
+
+cd "$ROOT_DIR/mock/ft"
+cargo near build non-reproducible-wasm 1>&2
+
+cd "$ROOT_DIR/contract/market"
+cargo near build non-reproducible-wasm 1>&2
+
+cd "$ROOT_DIR"
+export TEST_CONTRACTS_PREBUILT=1
--- script/test.sh
+++ script/test.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source ./prebuild-test-contracts.sh
 
-cd "$ROOT_DIR/mock/oracle"
-cargo near build non-reproducible-wasm
-
-cd "$ROOT_DIR/mock/ft"
-cargo near build non-reproducible-wasm
-
-cd "$ROOT_DIR/contract/market"
-cargo near build non-reproducible-wasm
-
-cd "$ROOT_DIR"
-export TEST_CONTRACTS_PREBUILT=1
 cargo nextest run "$@"
