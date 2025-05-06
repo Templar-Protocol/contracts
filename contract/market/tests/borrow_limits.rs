e9b3e82ff1f80de435cf0137de7a390b976075e6
peer2 <peer2@templarprotocol.com>
2025-05-07 02:07:32 +0900

    fix: apply borrow maximum amount properly

--- contract/market/tests/borrow_limits.rs
+++ contract/market/tests/borrow_limits.rs
@@ -2,14 +2,16 @@ use rstest::rstest;
 use test_utils::*;
 
 #[rstest]
-#[case(0, 1, u128::MAX)]
-#[case(1, 1, u128::MAX)]
-#[case(10, 10, 10)]
-#[case(0, 100, 100)]
-#[case(0, 50, 100)]
-#[case(100, 100, 100)]
+#[case(0, &[1], u128::MAX)]
+#[case(1, &[1], u128::MAX)]
+#[case(10, &[10], 10)]
+#[case(0, &[20, 20, 20, 20, 20], 100)]
 #[tokio::test]
-async fn borrow_within_bounds(#[case] minimum: u128, #[case] amount: u128, #[case] maximum: u128) {
+async fn borrow_within_bounds(
+    #[case] minimum: u128,
+    #[case] amounts: &[u128],
+    #[case] maximum: u128,
+) {
     let SetupEverything {
         c,
         supply_user,
@@ -23,7 +25,10 @@ async fn borrow_within_bounds(#[case] minimum: u128, #[case] amount: u128, #[cas
 
     c.supply(&supply_user, 1000).await;
     c.collateralize(&borrow_user, 2000).await;
-    c.borrow(&borrow_user, amount).await;
+
+    for amount in amounts {
+        c.borrow(&borrow_user, *amount).await;
+    }
 }
 
 #[rstest]
@@ -51,13 +56,21 @@ async fn borrow_below_minimum(#[case] minimum: u128, #[case] amount: u128, #[cas
 }
 
 #[rstest]
-#[case(0, 2, 1)]
-#[case(0, 1001, 1000)]
-#[case(1000, 1001, 1000)]
-#[case(100, 1001, 500)]
+#[case(0, &[2], 1)]
+#[case(0, &[1, 1], 1)]
+#[case(0, &[1001], 1000)]
+#[case(0, &[1000, 1], 1000)]
+#[case(1000, &[1001], 1000)]
+#[case(500, &[500, 501], 1000)]
+#[case(100, &[1001], 500)]
+#[case(100, &[100, 100, 100, 100, 100, 100, 100], 500)]
 #[tokio::test]
 #[should_panic = "Smart contract panicked: Borrow amount is greater than maximum allowed"]
-async fn borrow_above_maximum(#[case] minimum: u128, #[case] amount: u128, #[case] maximum: u128) {
+async fn borrow_above_maximum(
+    #[case] minimum: u128,
+    #[case] amounts: &[u128],
+    #[case] maximum: u128,
+) {
     let SetupEverything {
         c,
         supply_user,
@@ -69,7 +82,10 @@ async fn borrow_above_maximum(#[case] minimum: u128, #[case] amount: u128, #[cas
     })
     .await;
 
-    c.supply(&supply_user, 1000).await;
+    c.supply(&supply_user, 10000).await;
     c.collateralize(&borrow_user, 2000).await;
-    c.borrow(&borrow_user, amount).await;
+
+    for amount in amounts {
+        c.borrow(&borrow_user, *amount).await;
+    }
 }
