41b49749bcd411ac3530ad70ad1bd0391db1b007
peer2 <peer2@templarprotocol.com>
2025-09-04 09:45:10 +0100

    fix: don't cargo doc directly to _site/doc dir

--- script/build-docs.sh
+++ script/build-docs.sh
@@ -4,6 +4,7 @@ set -ex
 SCRIPT_DIR=$(dirname "$(readlink -f ${BASH_SOURCE[0]})")
 ROOT_DIR=$(readlink -f "$SCRIPT_DIR/..")
 
+rm -rf $ROOT_DIR/_site
 mkdir -p $ROOT_DIR/_site/{guide,doc}
 
 echo "Building guide..."
@@ -12,6 +13,7 @@ mdbook build --dest-dir $ROOT_DIR/_site/guide
 
 echo "Building Rust documentation..."
 cd $ROOT_DIR
-cargo doc --workspace --no-deps --target-dir $ROOT_DIR/_site/doc
+cargo doc --workspace --no-deps
+cp -r $ROOT_DIR/target/doc $ROOT_DIR/_site/doc
 
 echo "docs.templarfi.org" > $ROOT_DIR/_site/CNAME
