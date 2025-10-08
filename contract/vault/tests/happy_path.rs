40c1dbed095877dd25dc8927c8ac1f93e682b29b
carrion256 <carrion256@proton.me>
2025-10-08 09:36:04 +0100

    test: happy path with withdraw

--- contract/vault/tests/happy_path.rs
+++ contract/vault/tests/happy_path.rs
@@ -1,9 +1,17 @@
 use near_sdk::json_types::U128;
+use templar_common::{interest_rate_strategy::InterestRateStrategy, number::Decimal};
 use test_utils::{setup_test, setup_test_w, ContractController};
 
 #[tokio::test]
 async fn happy() {
-    setup_test!(extract(vault, c, vault_curator) accounts(supply_user));
+    setup_test!(
+        extract(vault, c, vault_curator)
+        accounts(supply_user, borrow_user)
+        config(|c| {
+            c.borrow_interest_rate_strategy =
+                InterestRateStrategy::linear(Decimal::ZERO, Decimal::ZERO).unwrap();
+        })
+    );
 
     c.init_account(&supply_user).await;
     vault.init_account(&supply_user).await;
@@ -12,9 +20,12 @@ async fn happy() {
     let amount: U128 = 1000.into();
 
     vault.supply(&supply_user, amount.0).await;
+    c.collateralize(&borrow_user, 2000).await;
 
     let weights = vec![(c.market.contract().id().clone(), U128(1))];
-    vault.allocate(&vault_curator, weights, Some(amount)).await;
+    vault
+        .allocate(&vault_curator, weights.clone(), Some(amount))
+        .await;
 
     assert_eq!(
         c.borrow_asset.balance_of(vault.contract().id()).await,
@@ -64,7 +75,9 @@ async fn happy() {
     );
 
     let user_balance = c.borrow_asset.balance_of(supply_user.id()).await;
+
     vault.withdraw(&supply_user, amount, None).await;
+
     assert_eq!(
         c.borrow_asset.balance_of(supply_user.id()).await,
         amount.0 + user_balance,
@@ -76,4 +89,34 @@ async fn happy() {
         supply_position.is_none(),
         "Supply position should be closed"
     );
+
+    c.storage_deposits(vault.contract().as_account()).await;
+
+    // Resupply and wait
+    vault.supply(&supply_user, amount.0).await;
+    // FIXME:Storage issue:         Error: Error { repr: Custom { kind: Execution, error: ActionError(ActionError { index: Some(0), kind: FunctionCallError(ExecutionError("Smart contract panicked: Storage error: Account vault0251007104533-70674114756315 has insufficient balance: 0.005 NEAR available, but attempted to use 0.008 NEAR")) }) } }
+    vault.allocate(&vault_curator, weights, Some(amount)).await;
+    while !c
+        .get_supply_position(v)
+        .await
+        .unwrap()
+        .get_deposit()
+        .incoming
+        .is_empty()
+    {
+        // TODO: should also do this in allocate
+        c.harvest_yield(vault.contract().as_account(), None, None)
+            .await;
+    }
+
+    println!(
+        "Balance of the market for the collateral asset: {}",
+        c.borrow_asset.balance_of(c.market.contract().id()).await
+    );
+
+    c.borrow(&borrow_user, 500).await;
+
+    // TODO: what happens if we try to withdraw now?
+    //
+    vault.withdraw(&supply_user, amount, None).await;
 }
