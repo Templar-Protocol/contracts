b4235f18fe62d08fd2e555d2a767c21c47e20de4
carrion256 <carrion256@proton.me>
2025-10-15 15:38:56 +0100

    ci: gas report for vault

--- script/ci/gas-report.sh
+++ script/ci/gas-report.sh
@@ -5,3 +5,4 @@ SCRIPT_DIR=$(dirname "$(readlink -f ${BASH_SOURCE[0]})")
 source "$SCRIPT_DIR/../prebuild-test-contracts.sh"
 
 cargo run --package templar-market-contract --example gas_report
+cargo run --package templar-vault-contract --example gas_report
