d0eb7a7dfe7c7e84a7d883f03a8cd96e4f0f8ead
peer2 <peer2@templarprotocol.com>
2025-10-29 06:30:13 +0000

    fix: script for alpha market deployments (#275)

---
+++ script/ci/alpha/deploy-market-from-registry.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR=$(dirname "$(readlink -f ${BASH_SOURCE[0]})")
+source "$SCRIPT_DIR/../utils.sh"
+
+parse_args "--account:ACCOUNT_ID,--registry:REGISTRY_ID,--version-key:VERSION_KEY,--init-args:INIT_ARGS,--name:NAME,--with-full-access-key:WITH_FULL_ACCESS_KEY,--network:NETWORK,--private-key:PRIVATE_KEY" "$@"
+
+if [ -z "$NETWORK" ]; then
+    NETWORK="testnet"
+fi
+
+if [ -z "$NAME" ]; then
+    NAME="null"
+else
+    NAME='"'$NAME'"'
+fi
+
+if [ -z "$WITH_FULL_ACCESS_KEY" ]; then
+    FULL_ACCESS_KEYS="null"
+else
+    FULL_ACCESS_KEYS=$(jq --null-input --arg key "${WITH_FULL_ACCESS_KEY}" '[$key]')
+fi
+
+ARGS=$(jq --null-input \
+    --argjson name "${NAME}" \
+    --arg version_key "${VERSION_KEY}" \
+    --arg init_args "$(echo -n "${INIT_ARGS}" | base64 | tr -d \\n)" \
+    --argjson full_access_keys "${FULL_ACCESS_KEYS}" \
+    '$ARGS.named')
+
+echo "Generated deployment args"
+echo "${ARGS}" | jq .
+
+near contract call-function as-transaction "${REGISTRY_ID}" \
+    deploy_market \
+        json-args "${ARGS}" \
+        prepaid-gas '300.0 Tgas' \
+        attached-deposit '6 NEAR' \
+    sign-as "${ACCOUNT_ID}" \
+    network-config "${NETWORK}" \
+    sign-with-plaintext-private-key "${PRIVATE_KEY}" \
+    send
+
+
+echo "Done"
