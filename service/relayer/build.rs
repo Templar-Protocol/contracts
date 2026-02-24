6b5121b45f369140b310d186975ced3960476e03
peer2 <peer2@templarprotocol.com>
2026-02-24 10:43:45 +0000

    chore: add npm install to build.rs

--- service/relayer/build.rs
+++ service/relayer/build.rs
@@ -3,9 +3,18 @@
 fn main() {
     println!("cargo::rerun-if-changed=redstone-bridge/");
 
-    let mut npm = std::process::Command::new("npm");
-    npm.arg("run");
-    npm.arg("build");
-    npm.current_dir("redstone-bridge");
-    npm.spawn().unwrap().wait().unwrap();
+    let install = std::process::Command::new("npm")
+        .arg("install")
+        .current_dir("redstone-bridge")
+        .status()
+        .unwrap();
+    assert!(install.success());
+
+    let build = std::process::Command::new("npm")
+        .arg("run")
+        .arg("build")
+        .current_dir("redstone-bridge")
+        .status()
+        .unwrap();
+    assert!(build.success());
 }
