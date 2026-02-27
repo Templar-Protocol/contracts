27321b49cb4039d8f6f2ca4c4d12dd23cc192a82
peer2 <peer2@templarprotocol.com>
2026-02-27 06:36:45 +0000

    chore: include input String in MarketVersion ParseError

--- tools/harvest-static-yield/src/market_version.rs
+++ tools/harvest-static-yield/src/market_version.rs
@@ -51,21 +51,37 @@ impl MarketVersion {
 
 #[derive(thiserror::Error, Debug, Clone, PartialEq, Eq)]
 pub enum ParseError {
-    #[error("Missing separator index {0}")]
-    Separator(usize),
-    #[error("Failed to parse segment index {0}")]
-    Segment(usize),
+    #[error("Missing separator index {index} in input '{input}'")]
+    Separator { index: usize, input: String },
+    #[error("Failed to parse segment index {index} in input '{input}'")]
+    Segment { index: usize, input: String },
 }
 
 impl std::str::FromStr for MarketVersion {
     type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (major, tail) = s.split_once('.').ok_or(ParseError::Separator(0))?;
-        let major: u16 = major.parse().map_err(|_| ParseError::Segment(0))?;
-        let (minor, patch) = tail.split_once('.').ok_or(ParseError::Separator(1))?;
-        let minor: u16 = minor.parse().map_err(|_| ParseError::Segment(1))?;
-        let patch: u16 = patch.parse().map_err(|_| ParseError::Segment(2))?;
+        let (major, tail) = s.split_once('.').ok_or(ParseError::Separator {
+            index: 0,
+            input: s.to_string(),
+        })?;
+        let major: u16 = major.parse().map_err(|_| ParseError::Segment {
+            index: 0,
+            input: s.to_string(),
+        })?;
+        let (minor, patch) = tail.split_once('.').ok_or(ParseError::Separator {
+            index: 1,
+            input: s.to_string(),
+        })?;
+        let minor: u16 = minor.parse().map_err(|_| ParseError::Segment {
+            index: 1,
+            input: s.to_string(),
+        })?;
+        let patch: u16 = patch.parse().map_err(|_| ParseError::Segment {
+            index: 2,
+            input: s.to_string(),
+        })?;
+
         Ok(Self {
             major,
             minor,
