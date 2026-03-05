463f25a5253a04a7ff161769c128b21842fb0ce7
peer2 <peer2@templarprotocol.com>
2026-03-05 03:45:56 +0000

    chore: remove unnecessary trait bounds

--- service/relayer/src/client/oracle/spec/mod.rs
+++ service/relayer/src/client/oracle/spec/mod.rs
@@ -9,12 +9,12 @@ pub use redstone::*;
 
 pub trait Spec: Send + Sync + 'static {
     type FeedId: std::hash::Hash + std::fmt::Debug + std::cmp::Eq + Clone + Send + Sync;
-    type Error: std::error::Error + 'static + Send + Sync;
+    type Error: std::error::Error + Send + Sync;
 
     fn name() -> &'static str;
     fn refresh(&self) -> Duration;
     fn update_actions(
         &self,
         feed_ids: &[Self::FeedId],
-    ) -> impl std::future::Future<Output = Result<Vec<Action>, Self::Error>> + Send + Sync;
+    ) -> impl std::future::Future<Output = Result<Vec<Action>, Self::Error>> + Send;
 }
