a85da63fe368edc449c6a51dfaa62c42524d1bd9
peer2 <peer2@templarprotocol.com>
2026-07-02 09:45:58 +0100

    refactor(gateway-store): make operations identity-only
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)

--- gateway/store/migrations/20260421120000_operation_store.down.sql
+++ gateway/store/migrations/20260421120000_operation_store.down.sql
@@ -1,7 +1,1 @@
-DROP TABLE IF EXISTS gateway_operation_steps;
-
 DROP TABLE IF EXISTS gateway_operations;
-
-DROP TYPE IF EXISTS gateway_operation_step_state;
-
-DROP TYPE IF EXISTS gateway_operation_status;
