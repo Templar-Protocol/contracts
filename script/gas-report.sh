f0dc93a534dedef03bc107b91bddcda4afc7560f
peer2 <peer2@templarprotocol.com>
2026-07-06 23:18:53 +0100

    ci(artifacts): cover prebuild artifact callers
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)

--- script/gas-report.sh
+++ script/gas-report.sh
@@ -2,7 +2,7 @@
 set -e
 
 SCRIPT_DIR=$(dirname "$(readlink -f ${BASH_SOURCE[0]})")
-source "$SCRIPT_DIR/../prebuild-test-contracts.sh"
+source "$SCRIPT_DIR/prebuild-test-contracts.sh"
 
 cargo run --package templar-market-contract --example gas_report
 cargo run --package templar-vault-contract --example gas_report
